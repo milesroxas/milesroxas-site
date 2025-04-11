ce8baa0afccb319a5984061db0bd76bf4ffa3c21
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-04-11 15:53:32 -0400

    fix: force ipv4 dns resolution to fix vercel build error

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    // Force Node.js to use IPv4 DNS resolution
+    NODE_OPTIONS: '--dns-result-order=ipv4first',
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
