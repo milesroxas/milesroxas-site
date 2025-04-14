c4ca49aa794aa6c3d7bd619ec2d1fba0e10b02a6
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-04-13 21:20:54 -0400

    fixed build errors with media

--- src/components/Media/index.tsx
+++ src/components/Media/index.tsx
@@ -9,17 +9,12 @@ export const Media: React.FC<Props> = (props) => {
   const { className, htmlElement = 'div', resource } = props
 
   const isVideo = typeof resource === 'object' && resource?.mimeType?.includes('video')
-  const Tag = htmlElement || Fragment
 
-  return (
-    <Tag
-      {...(htmlElement !== null
-        ? {
-            className,
-          }
-        : {})}
-    >
-      {isVideo ? <VideoMedia {...props} /> : <ImageMedia {...props} />}
-    </Tag>
-  )
+  if (htmlElement === null) {
+    return isVideo ? <VideoMedia {...props} /> : <ImageMedia {...props} />
+  }
+
+  const content = isVideo ? <VideoMedia {...props} /> : <ImageMedia {...props} />
+
+  return React.createElement(htmlElement, { className }, content)
 }
