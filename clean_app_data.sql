1e346a654f12c5845e6f510438f98ec296a0d2cc
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-04-18 19:43:02 -0400

    db issues

---
+++ clean_app_data.sql
@@ -0,0 +1,37 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.17 (Homebrew)
+-- Dumped by pg_dump version 14.17 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP SCHEMA IF EXISTS public;
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA public;
+
+
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON SCHEMA public IS 'standard public schema';
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- src/app/(frontend)/works/[slug]/page.client.tsx
+++ src/app/(frontend)/works/[slug]/page.client.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useHeaderTheme } from '@/providers/HeaderTheme'
 import React, { useEffect } from 'react'
-import { PageTransition } from '@/components/PageTransition'
 
 const PageClient: React.FC = () => {
   /* Force the header to be dark mode while we have an image behind it */
