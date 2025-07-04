3759152f022367362cbf2fbf854908f3cdead685
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-07-04 17:59:42 -0400

    fix: removed isDev flag

--- next.config.js
+++ next.config.js
@@ -34,7 +34,6 @@ const nextConfig = {
         pathname: '/api/media/file/**',
       },
     ],
-    unoptimized: isDev,
   },
 
   turbopack: {
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,45 +1,9 @@
-import * as migration_20250420_233710 from './20250420_233710';
-import * as migration_20250423_151516 from './20250423_151516';
-import * as migration_20250428_212335 from './20250428_212335';
-import * as migration_20250526_003733 from './20250526_003733';
-import * as migration_20250608_205651 from './20250608_205651';
-import * as migration_20250619_202147 from './20250619_202147';
-import * as migration_20250619_202917 from './20250619_202917';
+import * as migration_20250704_215356 from './20250704_215356';
 
 export const migrations = [
   {
-    up: migration_20250420_233710.up,
-    down: migration_20250420_233710.down,
-    name: '20250420_233710',
-  },
-  {
-    up: migration_20250423_151516.up,
-    down: migration_20250423_151516.down,
-    name: '20250423_151516',
-  },
-  {
-    up: migration_20250428_212335.up,
-    down: migration_20250428_212335.down,
-    name: '20250428_212335',
-  },
-  {
-    up: migration_20250526_003733.up,
-    down: migration_20250526_003733.down,
-    name: '20250526_003733',
-  },
-  {
-    up: migration_20250608_205651.up,
-    down: migration_20250608_205651.down,
-    name: '20250608_205651',
-  },
-  {
-    up: migration_20250619_202147.up,
-    down: migration_20250619_202147.down,
-    name: '20250619_202147',
-  },
-  {
-    up: migration_20250619_202917.up,
-    down: migration_20250619_202917.down,
-    name: '20250619_202917'
+    up: migration_20250704_215356.up,
+    down: migration_20250704_215356.down,
+    name: '20250704_215356'
   },
 ];
