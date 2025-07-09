5a0e3d118bad39203abb9a5b9fb6844e830afe8c
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-07-08 21:57:25 -0400

    created migration file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250704_215356 from './20250704_215356';
+import * as migration_20250709_015705 from './20250709_015705';
 
 export const migrations = [
   {
     up: migration_20250704_215356.up,
     down: migration_20250704_215356.down,
-    name: '20250704_215356'
+    name: '20250704_215356',
+  },
+  {
+    up: migration_20250709_015705.up,
+    down: migration_20250709_015705.down,
+    name: '20250709_015705'
   },
 ];
