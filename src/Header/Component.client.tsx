755930fef4ad6d35267a019423546bbe950add95
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-07-12 10:39:39 -0400

    clean up on header

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
 
 import type { Header } from '@/payload-types'
 
@@ -14,16 +12,9 @@ interface HeaderClientProps {
 
 export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   /* Storing the value in a useState to avoid hydration errors */
-  const [theme, setTheme] = useState<string | null>(null)
-
-  const pathname = usePathname()
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname])
 
   return (
-    <header {...(theme ? { 'data-theme': theme } : {})}>
+    <header>
       <HeaderNav data={data} />
     </header>
   )
