b535e1d2d97683bab06ade98238b5351d0d3cdc1
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-07-14 14:44:30 -0400

    fix: collection archive block app error

--- src/blocks/ArchiveBlock/ArchiveBlockClient.tsx
+++ src/blocks/ArchiveBlock/ArchiveBlockClient.tsx
@@ -8,20 +8,29 @@ import type { ArchiveBlock as ArchiveBlockProps } from '@/payload-types'
 import { CollectionArchive } from '@/components/CollectionArchive'
 import { CardWorkData } from '@/components/Card/Works/Component'
 import { CardPostData } from '@/components/Card/Posts/Component'
+import { cn } from '@/utilities/ui'
 export default function ArchiveBlockClient({
   theme,
   introContent,
   posts,
   works,
+  cardStyle,
 }: {
   theme: 'light' | 'dark' | 'system' | undefined | null
   introContent?: ArchiveBlockProps['introContent']
   posts?: CardPostData[]
   works?: CardWorkData[]
+  cardStyle?: 'card' | 'featured'
 }) {
   const appliedTheme = useBlockTheme(theme)
   return (
-    <div data-theme={appliedTheme} className="bg-background text-foreground pt-32 pb-36">
+    <div
+      data-theme={appliedTheme}
+      className={cn(
+        'bg-background text-foreground pt-32 pb-36',
+        cardStyle === 'featured' && 'bg-red-500',
+      )}
+    >
       {introContent && (
         <div className="container mb-16 px-8 md:px-20">
           <RichText
