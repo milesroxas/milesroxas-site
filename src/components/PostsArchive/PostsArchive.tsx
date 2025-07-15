b12fbcf74cecf3d782ee5af0854d2552ef93a542
Miles Roxas <132398659+sas-miles@users.noreply.github.com>
2025-07-15 14:00:24 -0400

    styles: spacing on archives

--- src/components/PostsArchive/PostsArchive.tsx
+++ src/components/PostsArchive/PostsArchive.tsx
@@ -13,7 +13,7 @@ export const PostsArchive: React.FC<Props> = (props) => {
   return (
     <div className={cn('container px-8 md:px-14 lg:px-20')}>
       <div>
-        <div className="grid grid-cols-4 gap-x-4 gap-y-4 sm:grid-cols-8 lg:gap-x-8 lg:gap-y-8 xl:gap-x-8">
+        <div className="grid grid-cols-4 gap-x-4 gap-y-20 sm:grid-cols-8 lg:gap-x-8 lg:gap-y-8 xl:gap-x-8">
           {posts?.map((result, index) => {
             if (typeof result === 'object' && result !== null) {
               return (
--- src/components/WorkArchive/index.tsx
+++ src/components/WorkArchive/index.tsx
@@ -13,7 +13,7 @@ export const WorkArchive: React.FC<Props> = (props) => {
   return (
     <div className={cn('container')}>
       <div>
-        <div className="grid grid-cols-4 gap-x-4 gap-y-4 sm:grid-cols-8 md:grid-cols-8 lg:gap-x-8 lg:gap-y-8 xl:gap-x-8">
+        <div className="grid grid-cols-4 gap-x-4 gap-y-20 sm:grid-cols-8 lg:gap-x-8 lg:gap-y-8 xl:gap-x-8">
           {works?.map((result, index) => {
             if (typeof result === 'object' && result !== null) {
               return (
